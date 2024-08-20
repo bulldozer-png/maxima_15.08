f7d1657580e7a743b4e82141374550e04c94bd9e
Георгий Бочевар <bochevar915@gmail.com>
2024-08-20 21:35:36 +0300

    Task2

--- src/hw1/Task1.java
+++ src/hw1/Task1.java
@@ -2,9 +2,11 @@ package hw1;
 
 public class Task1 {
     public static void main(String[] args) {
+
         double a = 5.0;
         double b = a / 2;
 
         System.out.println(b);
+
     }
 }
---
+++ src/hw1/Task2.java
@@ -0,0 +1,13 @@
+package hw1;
+
+public class Task2 {
+    public static void main(String[] args) {
+
+        int a = 50;
+        int b = 20;
+        int s = a * b; //Прямоугольник
+
+        System.out.println(s);
+
+    }
+}
