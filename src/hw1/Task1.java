dca4f5415dce2195e018ec3f64c1d694863d0aaa
Георгий Бочевар <bochevar915@gmail.com>
2024-08-20 21:27:42 +0300

    Task1

--- src/hw1/Task1.java
+++ src/hw1/Task1.java
@@ -2,7 +2,7 @@ package hw1;
 
 public class Task1 {
     public static void main(String[] args) {
-        double a = 5;
+        double a = 5.0;
         double b = a / 2;
 
         System.out.println(b);
