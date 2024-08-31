105546db38c96814c5352be85f69c8402ded6404
Георгий Бочевар <bochevar915@gmail.com>
2024-08-31 17:25:04 +0300

    31.08

--- src/hw3/Task3.java
+++ src/hw3/Task3.java
@@ -12,7 +12,10 @@ public class Task3 {
         int num3 = scanner.nextInt();
 
 
-        if ((num1 <= num2 && num1 >= num3) || (num1 >= num2 && num1 <= num3)) {
+        if (num1 == num2 && num2 == num3) {
+            System.out.println("Все числа между собой равны. Вывод: " + num1);
+
+        } else if ((num1 <= num2 && num1 >= num3) || (num1 >= num2 && num1 <= num3)) {
             System.out.println("Среднее число " + num1);
 
         } else if ((num2 <= num1 && num2 >= num3) || (num2 >= num1 && num2 <= num3)) {
@@ -20,10 +23,9 @@ public class Task3 {
 
         } else if ((num3 <= num1 && num3 >= num2) || (num3 >= num1 && num3 <= num2)) {
             System.out.println("Среднее число " + num3);
-
-        } else if (num1 == num2 && num2 == num3) {
-            System.out.println("Все числа между собой равны. Вывод: " + num1);
         }
 
     }
 }
+
+
