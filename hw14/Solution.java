be03da0367f443e99db24f39f4ede9aae3715859
Георгий Бочевар <bochevar915@gmail.com>
2024-10-20 13:36:01 +0300

    Коллекции 2 (20.10)
    * Заменил название переменных
    * Воспользовался Stream.iterate()

--- hw14/Solution.java
+++ hw14/Solution.java
@@ -2,27 +2,30 @@ package homework.hw14;
 
 import java.util.HashSet;
 import java.util.Set;
+import java.util.stream.Stream;
 
 public class Solution {
 
-    public static Set<Integer> createSet(){
+    public static Set<Integer> createSet() {
         Set<Integer> numbers = new HashSet<>();
-        for (int i = 0; i < 20; i++) {
-            numbers.add(i);
-        }
+//        for (int i = 0; i < 20; i++) {
+//            numbers.add(i);
+//        }
+        Stream.iterate(0, i -> i + 1).limit(20).forEach(numbers::add);
+
         return numbers;
     }
 
-    public static Set<Integer> removeAllNumbersGreaterThan10(Set<Integer> sset){
-        sset.removeIf(num -> num > 10);
-        return sset;
+    public static Set<Integer> removeAllNumbersGreaterThan10(Set<Integer> numbers) {
+        numbers.removeIf(num -> num > 10);
+        return numbers;
     }
 
 
     public static void main(String[] args) {
 
-        Set<Integer> fullSet = createSet();
-        removeAllNumbersGreaterThan10(fullSet);
+        Set<Integer> numbers = createSet();
+        removeAllNumbersGreaterThan10(numbers);
 
     }
 }
